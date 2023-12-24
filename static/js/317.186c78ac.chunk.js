7e167c865c7148955236b4b290fb2f488674da1a
rmlyaaa <126497199+rmlyaaa@users.noreply.github.com>
2023-12-24 11:20:10 +0000

    Deploying to gh-pages from @ rmlyaaa/goit-react-hw-05-movies@4160d8fff57471b268378bb5f76b482b4b259ef8 🚀

---
+++ static/js/317.186c78ac.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[317],{317:function(e,t,n){n.r(t),n.d(t,{default:function(){return o}});var r=n(87),c=n(184);function o(){return(0,c.jsx)(c.Fragment,{children:(0,c.jsxs)("b",{children:["page not found, back ",(0,c.jsx)(r.rU,{to:"/",children:"link"})," to Home"]})})}}}]);
+//# sourceMappingURL=317.186c78ac.chunk.js.map
